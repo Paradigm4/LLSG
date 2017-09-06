1263ee908a6d185dae3804b7a47a0dc369049c7c
Alex Poliakov <apoliakov@paradigm4.com>
2017-09-06 19:57:13 -0400

    Update LLSG.h

--- LLSG.h
+++ LLSG.h
@@ -5,20 +5,20 @@
 * Copyright (C) 2008-2016 SciDB, Inc.
 * All Rights Reserved.
 *
-* llsg is a plugin for SciDB, an Open Source Array DBMS maintained
+* LLSG is a plugin for SciDB, an Open Source Array DBMS maintained
 * by Paradigm4. See http://www.paradigm4.com/
 *
 * LLSG is free software: you can redistribute it and/or modify
 * it under the terms of the AFFERO GNU General Public License as published by
 * the Free Software Foundation.
 *
-* faster_redimension is distributed "AS-IS" AND WITHOUT ANY WARRANTY OF ANY KIND,
+* LLSG is distributed "AS-IS" AND WITHOUT ANY WARRANTY OF ANY KIND,
 * INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
 * NON-INFRINGEMENT, OR FITNESS FOR A PARTICULAR PURPOSE. See
 * the AFFERO GNU General Public License for the complete license terms.
 *
 * You should have received a copy of the AFFERO GNU General Public License
-* along with faster_redimension.  If not, see <http://www.gnu.org/licenses/agpl-3.0.html>
+* along with LLSG.  If not, see <http://www.gnu.org/licenses/agpl-3.0.html>
 *
 * END_COPYRIGHT
 */
